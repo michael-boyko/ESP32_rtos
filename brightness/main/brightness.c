d5ae565b43a666d6924a9a3e3b061a29e2a92dcc
KozakMiksa <kozak.miksa@gmail.com>
2020-10-04 20:30:27 +0300

    with second ADC

--- brightness/main/brightness.c
+++ brightness/main/brightness.c
@@ -2,16 +2,30 @@
 
 void change_brightness_oled(void *screen) {
     double brightnes = 0;
+    int raw = 0;
 
     while (true) {
-        brightnes = (double) adc1_get_raw(ADC1_CHANNEL_0);
-        brightnes = (brightnes /  4080 * 255);
-        reinit_display((uint8_t) brightnes);
+        adc2_get_raw(ADC2_CHANNEL_7, ADC_WIDTH_BIT_12, &raw);
+        brightnes = (((double)raw) );
+        reinit_display((uint8_t) brightnes / 10);
         printf("%d\n",(int) brightnes);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);	
+        vTaskDelay(100 / portTICK_PERIOD_MS);   
     }
     printf("afdafa\n");
 }
+
+// void change_brightness_oled(void *screen) {
+//     double brightnes = 0;
+
+//     while (true) {
+//         brightnes = (double) adc1_get_raw(ADC1_CHANNEL_0);
+//         brightnes = (brightnes /  4080 * 255);
+//         reinit_display((uint8_t) brightnes);
+//         printf("%d\n",(int) brightnes);
+//         vTaskDelay(10 / portTICK_PERIOD_MS);	
+//     }
+//     printf("afdafa\n");
+// }
 
 void app_main(void) {
     uint8_t **screen = create_arr();
--- serial_console/main/console.c
+++ serial_console/main/console.c
@@ -531,21 +531,21 @@ void app_main()
     };
     // gpio_set_direction(33, GPIO_MODE_OUTPUT);
     // gpio_set_level(33, 1);
-    gpio_set_pull_mode(33, GPIO_PULLUP_ONLY);
-    pulse_led_use_PWM(); // delete
+    // gpio_set_pull_mode(33, GPIO_PULLUP_ONLY);
+    // pulse_led_use_PWM(); // delete
     
-    // uart_driver_install(UART_NUM, 2048, 2048, 20, &uart0_queue, 0);
-    // uart_param_config(UART_NUM, &uart_config);
+    uart_driver_install(UART_NUM, 2048, 2048, 20, &uart0_queue, 0);
+    uart_param_config(UART_NUM, &uart_config);
 
-    // uart_set_pin(UART_NUM, 17, 16, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
-    // uart_pattern_queue_reset(UART_NUM, 20);
+    uart_set_pin(UART_NUM, 17, 16, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+    uart_pattern_queue_reset(UART_NUM, 20);
 
     // led_queue = xQueueCreate( 10, sizeof( struct led_on_off * ) );
     // pulse_queue = xQueueCreate( 10, sizeof( struct led_pulse * ) );
     // if (pulse_queue == NULL)
     //     printf("pulse_queue ------\n");
 
-    // xTaskCreate(task_read_bytes, "task_read_bytes", 12048, NULL, 10, NULL);
+    xTaskCreate(task_read_bytes, "task_read_bytes", 12048, NULL, 10, NULL);
     // xTaskCreate(pulse_led_use_PWM, "pulse_led_use_PWM", 12048, NULL, 10, NULL);
     // xTaskCreate(led_on_off, "led_on_off", 1048, NULL, 10, NULL);
 
